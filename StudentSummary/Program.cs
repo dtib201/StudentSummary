83f6df07ff255705160064b12777136fab658dc2
Daniel Min Tibbotts <dtib201@mywhitecliffe.com>
2021-02-23 12:21:30 +1300

    Create VS Solution

---
+++ StudentSummary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentSummary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
